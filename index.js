d8fb23126195abec2bc84bdae4e03add028a842f
islamemon59 <islamemon59@gmail.com>
2025-10-02 21:35:15 +0600

    games data api added

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-
-
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(process.env.MONGODB_URI, {
   serverApi: {
@@ -24,19 +22,29 @@ const client = new MongoClient(process.env.MONGODB_URI, {
 
 async function run() {
   try {
+    const db = client.db("gameCollection");
+    const gameDataCollection = db.collection("gameData");
+
+    app.get("/games", async (req, res) => {
+      try {
+        const games = await gameDataCollection.find().toArray();
+        res.json(games);
+      } catch (error) {
+        res.status(500).json({ message: "Error fetching games", error });
+      }
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-
   }
 }
 run().catch(console.dir);
 
 // Basic route
-app.get("/", (req, res) => {
+app.get("/game", (req, res) => {
   res.send("Hello, Express Server is running!");
 });
 
